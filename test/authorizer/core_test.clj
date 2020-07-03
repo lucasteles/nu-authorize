04ddca558ae3e1b2cad0c1064cc1b2418b46b001
Lucas Teles <lucas.teles@lambda3.com.br>
2020-07-03 17:37:43 -0300

    teste integrado basico

--- test/authorizer/core_test.clj
+++ test/authorizer/core_test.clj
@@ -8,15 +8,13 @@
 (defn join-as-lines [in] (string/join "\n" in))
 (defn interact [args] (string/split-lines (with-out-str (with-in-str (join-as-lines args) (app/-main)))))
 
-(interact [json json])
-
 (deftest main-test
   (testing "should process the base testcase"
-           (let [input ["{ \"account\": { \"activeCard\": true, \"availableLimit\": 100 } }" 
-                        "{ \"transaction\": { \"merchant\": \"Burger King\", \"amount\": 20, \"time\": \"2019-02-13T10:00:00.000Z\" } }" 
-                        "{ \"transaction\": { \"merchant\": \"Habbib's\", \"amount\": 90, \"time\": \"2019-02-13T11:00:00.000Z\" } }"]
-                 output ["{\"account\":{\"activeCard\":true,\"availableLimit\":100},\"violations\":[]}" 
-                         "{\"account\":{\"activeCard\":true,\"availableLimit\":80},\"violations\":[]}" 
-                         "{\"account\":{\"activeCard\":true,\"availableLimit\":80},\"violations\":[\"insufficient-limit\"]}"]
-                 result (interact input)]
-          (is (= output result ))))
+    (let [input ["{ \"account\": { \"activeCard\": true, \"availableLimit\": 100 } }"
+                 "{ \"transaction\": { \"merchant\": \"Burger King\", \"amount\": 20, \"time\": \"2019-02-13T10:00:00.000Z\" } }"
+                 "{ \"transaction\": { \"merchant\": \"Habbib's\", \"amount\": 90, \"time\": \"2019-02-13T11:00:00.000Z\" } }"]
+          output ["{\"account\":{\"activeCard\":true,\"availableLimit\":100},\"violations\":[]}"
+                  "{\"account\":{\"activeCard\":true,\"availableLimit\":80},\"violations\":[]}"
+                  "{\"account\":{\"activeCard\":true,\"availableLimit\":80},\"violations\":[\"insufficient-limit\"]}"]
+          result (interact input)]
+      (is (= output result)))))
